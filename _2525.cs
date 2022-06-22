46e21b5c3e77aeec0f8c30a5c69ff15a53d064fe
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-22 20:40:13 +0900

    Add files via upload

---
+++ _2525.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2525
+{
+    class _2525
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int c = int.Parse(Console.ReadLine());
+
+            int a = int.Parse(s[0]);
+            int b = int.Parse(s[1]) + c;
+
+            while (b >= 60)
+            {
+                a++;
+                b -= 60;
+                if (a >= 24)
+                    a -= 24;
+            }
+            Console.WriteLine("{0} {1}", a,b);
+        }
+    }
+}
