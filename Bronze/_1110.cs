1776968b0db55256c5561be30b8364fde6e65e45
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-23 21:00:58 +0900

    Add files via upload

---
+++ Bronze/_1110.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Baekjoon
+{
+    class _1110
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num2 = num;
+            int sum = 0;
+
+            while (true)
+            {
+                int a = (num2 % 10) * 10;
+                int b = (num2 / 10 + num2 % 10) % 10;
+                num2 = a + b;
+                sum++;
+
+                if (num2 == num)
+                    break;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
