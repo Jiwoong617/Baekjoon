de4444975d70e8977ba13355120d42f993cd1edb
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-25 15:12:50 +0900

    Add files via upload

---
+++ Bronze/_4344.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon
+{
+    class _4344
+    {
+        static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine());
+            string[][] a = new string[size][];
+            double[] num = new double[size];
+
+            for (int i = 0; i < size; i++)
+                a[i] = Console.ReadLine().Split();
+
+            for (int i = 0; i < size; i++)
+            {
+                int per = 0;
+
+                for (int j = 1; j <= Convert.ToInt32(a[i][0]); j++)
+                {
+                    num[i] += Convert.ToDouble(a[i][j]);
+                }
+                num[i] /= Convert.ToInt32(a[i][0]);
+
+                for (int k = 1; k <= Convert.ToInt32(a[i][0]); k++)
+                {
+                    if (Convert.ToDouble(a[i][k]) > num[i])
+                        per += 1;
+                }
+                num[i] = (double)per/Convert.ToDouble(a[i][0]) * 100;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("{0:#0.000}%",num[i]);
+            }
+               
+        }
+    }
+}
