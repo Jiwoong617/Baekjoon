55f5e82848c81e93f8c486b3a905d25f492fc75d
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-27 17:33:37 +0900

    Add files via upload

---
+++ Silver/_1065.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon
+{
+    class _1065
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (Hansu(i.ToString()))
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static bool Hansu(string n)
+        {
+            if (n.Length < 3)
+                return true;
+
+            int[] a = new int[n.Length];
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                a[i] = int.Parse(n[i].ToString()); //또는 Convert.ToInt32(n[i]);
+            }
+
+            if ((a[0] - a[1]) == (a[1] - a[2]))
+                return true;
+            else
+                return false;
+        }
+    }
+}
