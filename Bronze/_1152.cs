c2f6210b793137eb1464adb46890321c9aa2bd35
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-28 17:37:02 +0900

    Add files via upload

---
+++ Bronze/_1152.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon
+{
+    class _1152
+    {
+        static void Main(string[] args)
+        {
+            string[] a = Console.ReadLine().Split();
+            if (a[0] == "" && a[a.Length-1] == "")
+                Console.WriteLine(a.Length - 2);
+            else if (a[0] == "" || a[a.Length - 1] == "")
+                Console.WriteLine(a.Length-1);
+            else
+                Console.WriteLine(a.Length);
+        }
+    }
+}
