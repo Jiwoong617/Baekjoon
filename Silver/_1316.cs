2f042380d6438187a36d8eb4f1da058d9484854a
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-29 17:25:41 +0900

    Add files via upload

---
+++ Silver/_1316.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon.Silver
+{
+    class _1316
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i<n; i++)
+            {
+                string str = Console.ReadLine();
+                bool grp = true;
+                for(int j = 0; j<str.Length; j++)
+                {
+                    int a = str.IndexOf(str[j]);
+                    int b = str.LastIndexOf(str[j]);
+                    while (a < b)
+                    {
+                        if(str[a] != str[a + 1])
+                        {
+                            grp = false;
+                            break;
+                        }
+                        a++;
+                    }
+                    if (!grp)
+                        break;
+                }
+                if (grp)
+                    count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
