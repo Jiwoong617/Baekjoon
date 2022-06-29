147f44853e196c14c7bba74763c74435ee74c1a8
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-06-29 16:37:16 +0900

    Add files via upload

---
+++ Silver/_2941.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon.Silver
+{
+    class _2941
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+            for(int i=0; i<cro.Length; i++)
+                str = str.Replace(cro[i], "_");
+
+            Console.WriteLine(str.Length);
+        }
+    }
+}
