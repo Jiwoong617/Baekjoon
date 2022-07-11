6d9a693328942d6c37adc0c1c97a48b9141aa03f
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-07-11 16:45:50 +0900

    Create _10814.cs

---
+++ Silver/_10814.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Baekjoon.Silver
+{
+    class _10814
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            (int, string)[] arr = new (int, string)[num];
+            StringBuilder stb = new StringBuilder();
+            for(int i = 0; i<num; i++)
+            {
+                string[] str = Console.ReadLine().Split();
+                arr[i] = (int.Parse(str[0]), str[1]);
+            }
+
+            var ar = arr.OrderBy(x => x.Item1).ToArray();
+
+            for (int i = 0; i < num; i++)
+                stb.AppendLine($"{ar[i].Item1} {ar[i].Item2}");
+            Console.WriteLine(stb);
+        }
+    }
+}
