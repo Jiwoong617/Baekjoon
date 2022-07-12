f9e49c415b8a9e0e61a86e50c6456bd6eb15d5d5
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-07-12 15:20:12 +0900

    Update _11651.cs

--- Silver/_11651.cs
+++ Silver/_11651.cs
@@ -11,7 +11,7 @@ namespace Baekjoon.Silver
         {
             int num = int.Parse(Console.ReadLine());
             int[][] arr = new int[num][];
-            int[] a = new int[1];
+            
             for (int i = 0; i < num; i++)
             {
                 int[] n = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
