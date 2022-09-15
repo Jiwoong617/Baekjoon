0520d7d552dee2aa9c4137fafc3233b89288d00d
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2022-09-15 15:39:48 +0900

    Create _11279.cs

---
+++ 다시_풀기/_11279.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Baekjoon.silver
+{
+    class _11279
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stb = new StringBuilder();
+            int n = int.Parse(Console.ReadLine());
+            PriorityQueue<int, int> p_que = new PriorityQueue<int, int>();
+
+            for(int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num == 0 && p_que.Count < 1)
+                    stb.AppendLine("0");
+
+                else if (num == 0)
+                    stb.AppendLine(p_que.Dequeue().ToString());
+
+                else
+                    p_que.Enqueue(num, n-num);
+            }
+
+            Console.WriteLine(stb);
+        }
+    }
+}
