f2f7a0bf39d6396552536fa9ee23abc99b940b6f
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2023-05-04 10:31:35 +0900

    Create _9659.cs

---
+++ Silver/_9659.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Baekjoon
+{
+    internal class _9659
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(long.Parse(Console.ReadLine())% 2==0 ? "CY":"SK");
+        }
+    }
+}
